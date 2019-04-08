0d3bdffd60a5de313672cc4b9b7384f61163798f
Garrett Money <garrettmoney@icloud.com>
2019-04-07 19:59:07 -0500

    Update Player.py

--- Project/Garrett/Player.py
+++ Project/Garrett/Player.py
@@ -16,6 +16,23 @@ ROSYBROWN = (188,143,143)
 PALEVIOLET = (219,112,147)
 YELLOW = (255,255,0)
 
+# class PainBox(pygame.sprite.Sprite):
+
+#     # Constructor. Pass in the color of the block,
+#     # and its x and y position
+#     def __init__(self, color, damage, position, player):
+#        # Call the parent class (Sprite) constructor
+#        pygame.sprite.Sprite.__init__(self)
+
+#        # Create an image of the block, and fill it with a color.
+#        # This could also be an image loaded from the disk.
+#        self.image = pygame.Surface([width, height])
+#        self.image.fill(color)
+
+#        # Fetch the rectangle object that has the dimensions of the image
+#        # Update the position of this object by setting the values of rect.x and rect.y
+#        self.rect = self.image.get_rect()
+
 class Player(pygame.sprite.Sprite):
     """ This class represents the bar at the bottom that the player
         controls. """
@@ -31,6 +48,21 @@ class Player(pygame.sprite.Sprite):
         # This could also be an image loaded from the disk.
         self.width = 20
         self.height = 20
+
+        # genomeInputs = 3;
+        # genomeOutputs = 3;
+        # self.brain = genome(genomeInputs, genomeOutputs)
+
+        # # float[] vision = new float[genomeInputs];//the input array fed into the neuralNet 
+        # # float[] decision = new float[genomeOutputs]; //the out put of the NN 
+        # vision = []
+        # decision = []
+
+        # self.framesAlive = 0
+
+        # self.runningFitness
+        # self.totalFitnessCalculations
+        # self.avgFitness
 
         self.maxHealth = 40
         self.health = 40
@@ -89,6 +121,19 @@ class Player(pygame.sprite.Sprite):
         else:
             pygame.event.post(self.stopEvent)
     
+    def calculateFitness(self):
+      progressToGoal = self.distanceToPoint((800,500), False, RED, "X") / 500
+      timeAlive = self.framesAlive / 60
+      health = self.health / self.maxHealth
+      fitness = timeAlive - progressToGoal + health
+      self.runningFitness += fitness
+      self.totalFitnessCalculations += 1
+      return fitness
+    def determineAvgFitness(self):
+      return self.runningFitness / self.totalFtinessCalculations
+
+    # def look(self)
+
     def updateHealth(self):
         healthBarLength = (self.health/self.maxHealth) * self.width
         if self.health <= 0:
@@ -106,18 +151,6 @@ class Player(pygame.sprite.Sprite):
             else: 
                 pygame.draw.line(self.screen, RED, (self.rect.x, self.rect.y - 10), (self.rect.x + healthBarLength, self.rect.y - 10), 4)
 
-
-    # def displayDeath(self):
-        
-    # def determineAction(self, point):
-    #     if self.rect.x < point[0]:
-    #         self.executeAction(1)
-    #     if self.rect.x > point[0]:
-    #        self.executeAction(0)
-    #     if self.distanceToPoint(point) < 100:
-    #          self.executeAction(2)
-    #     else:
-    #         self.executeAction(3)
     
     def distanceToPoint(self, point, drawFlag = False, color = WHITE, axis = "BOTH"):
         x_goal = point[0]
@@ -166,8 +199,12 @@ class Player(pygame.sprite.Sprite):
     
 
     def update(self):
+
         #update neural network
         mouse_pos = pygame.mouse.get_pos()
+
+        #helps determine fitness
+        # self.framesAlive += 1
 
         #pass outputs to think
         self.think(mouse_pos)
@@ -179,6 +216,7 @@ class Player(pygame.sprite.Sprite):
         # Move left/right
         self.rect.x += self.change_x
 
+        # hit_box_list = pygame.sprite.spritecollide(self, self.level.danger_list, False)
 
         # See if we hit anything
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
@@ -283,3 +321,210 @@ class Player(pygame.sprite.Sprite):
             self.jump()
         elif action == 3:
             self.stop()
+
+
+
+
+""" ADD TO PLAYER CLASS
+
+  float fitness;
+  Genome brain;
+  boolean replay = false;
+
+  float unadjustedFitness;
+  int lifespan = 0;//how long the player lived for fitness
+  int bestScore =0;//stores the score achieved used for replay
+  boolean dead;
+  int score;
+  int gen = 0;
+
+  int genomeInputs = 7;
+  int genomeOutputs = 3;
+
+  float[] vision = new float[genomeInputs];//t he input array fed into the neuralNet 
+  float[] decision = new float[genomeOutputs]; //the out put of the NN 
+  //-------------------------------------
+  float posY = 0;
+  float velY = 0;
+  float gravity =1.2;
+  int runCount = -5;
+  int size = 20;
+
+  ArrayList<Obstacle> replayObstacles = new ArrayList<Obstacle>();
+  ArrayList<Bird> replayBirds = new ArrayList<Bird>();
+  ArrayList<Integer> localObstacleHistory = new ArrayList<Integer>();
+  ArrayList<Integer> localRandomAdditionHistory = new ArrayList<Integer>();
+  int historyCounter = 0;
+  int localObstacleTimer = 0;
+  float localSpeed = 10;
+  int localRandomAddition = 0;
+
+  boolean duck= false;
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------
+  //constructor
+
+  Player() {
+    brain = new Genome(genomeInputs, genomeOutputs);
+  }
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+
+
+
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------
+  //gets the output of the brain then converts them to actions
+  void think() {
+
+    float max = 0;
+    int maxIndex = 0;
+    //get the output of the neural network
+    decision = brain.feedForward(vision);
+
+    for (int i = 0; i < decision.length; i++) {
+      if (decision[i] > max) {
+        max = decision[i];
+        maxIndex = i;
+      }
+    }
+
+    if (max < 0.7) {
+      ducking(false);
+      return;
+    }
+
+    switch(maxIndex) {
+    case 0:
+      jump(false);
+      break;
+    case 1:
+      jump(true);
+      break;
+    case 2:
+      ducking(true);
+      break;
+    }
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------
+  //fot Genetic algorithm
+  void calculateFitness() {
+    fitness = score*score;
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------
+  Player crossover(Player parent2) {
+    Player child = new Player();
+    child.brain = brain.crossover(parent2.brain);
+    child.brain.generateNetwork();
+    return child;
+  }
+  //--------------------------------------------------------------------------------------------------------------------------------------------------------
+  //if replaying then the dino has local obstacles
+  void updateLocalObstacles() {
+    localObstacleTimer ++;
+    localSpeed += 0.002;
+    if (localObstacleTimer > minimumTimeBetweenObstacles + localRandomAddition) {
+      addLocalObstacle();
+    }
+    groundCounter ++;
+    if (groundCounter > 10) {
+      groundCounter =0;
+      grounds.add(new Ground());
+    }
+
+    moveLocalObstacles();
+    showLocalObstacles();
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------
+  void moveLocalObstacles() {
+    for (int i = 0; i< replayObstacles.size(); i++) {
+      replayObstacles.get(i).move(localSpeed);
+      if (replayObstacles.get(i).posX < -100) {
+        replayObstacles.remove(i);
+        i--;
+      }
+    }
+
+    for (int i = 0; i< replayBirds.size(); i++) {
+      replayBirds.get(i).move(localSpeed);
+      if (replayBirds.get(i).posX < -100) {
+        replayBirds.remove(i);
+        i--;
+      }
+    }
+    for (int i = 0; i < grounds.size(); i++) {
+      grounds.get(i).move(localSpeed);
+      if (grounds.get(i).posX < -100) {
+        grounds.remove(i);
+        i--;
+      }
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------------------------------------------------------
+  void addLocalObstacle() {
+    int tempInt = localObstacleHistory.get(historyCounter);
+    localRandomAddition = localRandomAdditionHistory.get(historyCounter);
+    historyCounter ++;
+    if (tempInt < 3) {
+      replayBirds.add(new Bird(tempInt));
+    } else {
+      replayObstacles.add(new Obstacle(tempInt -3));
+    }
+    localObstacleTimer = 0;
+  }
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------
+  void showLocalObstacles() {
+    for (int i = 0; i< grounds.size(); i++) {
+      grounds.get(i).show();
+    }
+    for (int i = 0; i< replayObstacles.size(); i++) {
+      replayObstacles.get(i).show();
+    }
+
+    for (int i = 0; i< replayBirds.size(); i++) {
+      replayBirds.get(i).show();
+    }
+  }
+}
+
+"""
+
+
+"""
+
+  //returns a clone of this player with the same brian
+  Player clone() {
+    Player clone = new Player();
+    clone.brain = brain.clone();
+    clone.fitness = fitness;
+    clone.brain.generateNetwork(); 
+    clone.gen = gen;
+    clone.bestScore = score;
+    return clone;
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  //since there is some randomness in games sometimes when we want to replay the game we need to remove that randomness
+  //this fuction does that
+
+  Player cloneForReplay() {
+    Player clone = new Player();
+    clone.brain = brain.clone();
+    clone.fitness = fitness;
+    clone.brain.generateNetwork();
+    clone.gen = gen;
+    clone.bestScore = score;
+    clone.replay = true;
+    if (replay) {
+      clone.localObstacleHistory = (ArrayList)localObstacleHistory.clone();
+      clone.localRandomAdditionHistory = (ArrayList)localRandomAdditionHistory.clone();
+    } else {
+      clone.localObstacleHistory = (ArrayList)obstacleHistory.clone();
+      clone.localRandomAdditionHistory = (ArrayList)randomAdditionHistory.clone();
+    }
+
+    return clone;
+  }
+"""
