3cbfbd35b64e47d15f7fa003dbf2af7d2ed4f6c1
Garrett Money <garrettmoney@icloud.com>
2019-04-16 14:48:27 -0500

    Sword.py

---
+++ Project/Garrett/Sword.py
@@ -0,0 +1,51 @@
+import pygame
+
+class Sword(pygame.sprite.Sprite):
+    def __init__(self,x,y,width,height,color,facing):
+
+        super().__init__()
+        self.facing = facing
+        self.x = x
+        self.y = y
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.width = width
+
+        self.timer = 0
+
+        if facing == -1:
+            self.rect.x = x - 50
+            self.rect.y = y + 30
+        elif facing == 1:
+            self.rect.x = x + 45#50
+            self.rect.y = y + 30
+        else:
+            self.rect.x = x + width
+            self.rect.y = y - 20
+
+    def update(self):
+        if self.facing == -1:
+            self.rect.x = self.x - 50
+            self.rect.y = self.y + 30
+        elif self.facing == 1:
+            self.rect.x = self.x + 45#50
+            self.rect.y = self.y + 30
+        else:
+            self.rect.x = self.x + self.width
+            self.rect.y = self.y - 20
+        self.timer += 1
+
+        if self.timer >= 3:
+            self.kill()
+
+
+
+        # for sword in attack_list:
+        #     sword.rect.x = self.x
+        #     sword.rect.y = self.y
+        # self.attack_list.add(sword)
+
+    # def draw(self, win):
+    #     pygame.draw.rect(win, self.color, pygame.Rect(self.x, self.y, 60, 20))
