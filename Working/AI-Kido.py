bf3983639610f861edc5f6a358fc5f01ba7c1187
Patrick <37915866+PRMorgan@users.noreply.github.com>
2019-04-28 19:14:47 -0500

    Fixed show buttons. Updated Sprites.

--- Working/AI-Kido.py
+++ Working/AI-Kido.py
@@ -90,6 +90,13 @@ def main():
                 gameUI.displayText(screen, timemsg,388,110,140,20, BLUE)
 
                 numGoalsMsg = str(game.player.numGoals - game.enemy.numGoals)
+                if showIndex == -1: #showAll
+                    pop.games[0].level.drawBG(pop.games[0])
+                    for game in pop.games:
+                        game.level.draw(game) #draw elements
+                else: #use index - show only that game
+                    pop.games[showIndex[0]].level.drawBG(pop.games[showIndex[0]])
+                    pop.games[showIndex[0]].level.draw(pop.games[showIndex[0]])
                 gameUI.displayText(screen, numGoalsMsg, 388, 175, 120, 20, False)
 
             # Limit to 60 frames per second
